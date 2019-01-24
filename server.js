2758cc56b1ae7749767c40d1bd71670c0ff9f521
Kaai Suzuki <suzydp@me.com>
2019-01-24 15:27:57 -0800

    create server.js

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5555;
-
-// test - get respond with "Hello world" when a GET request is made
-app.get('/', (req, res) => {
-  res.send('hello world')
-});
+const port = 3000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
