4c4c556a33ba496a7255bdf59b2a2988d18159d4
Kaai Suzuki <suzydp@me.com>
2019-01-24 16:49:49 -0800

    route in server.js

--- server.js
+++ server.js
@@ -4,4 +4,17 @@ const port = 3000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
 
+// route
+app.get('/', (req, res) => {
+  res.send('Hello, World');
+})
+
+app.get('/help', (req, res) => {
+  res.send('This is the help page what you want');
+})
+
+app.get('/something', (req, res) => {
+  res.send('Something will be shown');
+})
+
 // module.exports = router;
